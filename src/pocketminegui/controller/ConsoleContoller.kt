a7920a50b622ff935e38c71f723210023c2a563d
Rice_Field <famima65536@gmail.com>
2018-03-08 20:43:46 +0900

    アノテーション付きでのFXML

--- src/pocketminegui/controller/ConsoleContoller.kt
+++ src/pocketminegui/controller/ConsoleContoller.kt
@@ -1,5 +1,12 @@
 package pocketminegui.controller
 
+import javafx.fxml.FXML
+import javafx.scene.control.ListView
+import javafx.scene.control.TextField
+
 class ConsoleController() {
 
+    @FXML lateinit var console : ListView<String>
+    @FXML lateinit var commandField : TextField
+    
 }
